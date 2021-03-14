67f8539aa5a9e3df60a01b18c0d11cb4b08ba780
tako24 <frenk6795@gmail.com>
2021-03-14 17:27:52 +0500

    Simple dash and camera follow

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	public float speed = 5f;
-	public Camera cam;
 	private Vector2 movement;
 	private Vector2 mousePosition;
 	private Rigidbody2D rb;
+	private bool isDashBtnDown;
 
 	void Start()
 	{
@@ -21,24 +21,36 @@ public class PlayerController : MonoBehaviour
 	void FixedUpdate()
 	{
 		rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-		Vector2 lookDirection = mousePosition - rb.position;
+		Vector2 lookDirection = movement - rb.position;
 		float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
 		rb.rotation = angle;
+		if (isDashBtnDown)
+        {
+			float dashAmount = 5f;
+			rb.MovePosition(rb.position + movement * dashAmount);
+			isDashBtnDown = false;
+        }
 	}
 
-	//void LookAtCursor()
-	//{
-	//	Vector3 lookPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
-	//	lookPos = lookPos - transform.position;
-	//	float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
-	//	transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-	//}
+	void LookAtCursor()
+	{
+		Vector3 lookPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+		lookPos = lookPos - transform.position;
+		float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
 
 	void Update()
 	{
 		movement.x = Input.GetAxis("Horizontal");
 		movement.y = Input.GetAxis("Vertical");
 
-		mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+		LookAtCursor();
+
+
+		if (Input.GetKeyDown(KeyCode.E))
+        {
+			isDashBtnDown = true;
+        }
 	}
 }
