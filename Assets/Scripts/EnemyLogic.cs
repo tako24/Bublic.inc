ab0aa3d5f91cb83bf91467cdef610771efe950e6
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2021-05-02 19:59:13 +0500

    Летающий моб и моб, атакующий с дистанции

--- Assets/Scripts/EnemyLogic.cs
+++ Assets/Scripts/EnemyLogic.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
 using System.Linq;
+using System;
 
 public enum State
 {
     Patrooling,
-    Atack
+    Atack,
+    Stop,
+    MoveToPoint
+}
+public enum LogicType
+{
+    flyingMob,
+    distanceMob,
+    commonMob
 }
 public class EnemyLogic : MonoBehaviour
 {
@@ -15,41 +24,101 @@ public class EnemyLogic : MonoBehaviour
     private GameObject[] patroolPoints;
     public AIPath AIPath;
     public AIDestinationSetter AIDestinationSetter;
-    public State state;
+    private State state;
+    public LogicType LogicType;
     private GameObject player;
+    public float distanceToPlayer;
+    public float baseDistance;
+    public float runningAwayDistance=3.2f;
+    public float stoprunAwatDelta = 0.2f;
+    private GameObject[] flyingPoints;
     
 
     void Start()
     {
         patroolPoints = GameObject.FindGameObjectsWithTag("patrol").Where(x=>x.transform.parent.gameObject.GetHashCode()==gameObject.transform.parent.gameObject.GetHashCode()).ToArray();
+        flyingPoints= GameObject.FindGameObjectsWithTag("flypoint").Where(x => x.transform.parent.gameObject.GetHashCode() == gameObject.transform.parent.gameObject.GetHashCode()).ToArray();
         player = GameObject.Find("Player");
-        if (patroolPoints.Length != 0)
+        if (LogicType == LogicType.flyingMob)
+        {
+            state = State.Stop;
+            if(flyingPoints.Length!=0)
+                gameObject.transform.position = flyingPoints[UnityEngine.Random.Range(0, flyingPoints.Length)].transform.position;
+        }
+        if (patroolPoints.Length != 0 && LogicType != LogicType.flyingMob)
         {
             state = State.Patrooling;
             Patroling();
         }
     }
+    private void LookAtPlayer()
+    {
+
+        gameObject.transform.LookAt(player.transform);
+        AIPath.enableRotation = false;
+    }
     public void Atack()
     {
         state = State.Atack;
-        AIDestinationSetter.target = player.transform;
+        if(LogicType!=LogicType.distanceMob)
+            AIDestinationSetter.target = player.transform;
+        if (LogicType == LogicType.distanceMob)
+        {
+            gameObject.GetComponent<ShootSystem>().IsShooting = true;
+            LookAtPlayer();
+            AIDestinationSetter.target = player.transform;
+        }
+
     }
+
+
     public void StopAtack()
     {
-        state = State.Patrooling;
-        Patroling();
+        if (LogicType != LogicType.flyingMob)
+        {
+            state = State.Patrooling;
+            Patroling();
+        }
+        else state = State.Stop;
     }
     public void Patroling()
     {   
-        var target = patroolPoints[Random.Range(0, patroolPoints.Length - 1)];
+        var target = patroolPoints[UnityEngine.Random.Range(0, patroolPoints.Length - 1)];
         AIDestinationSetter.target = target.transform;
     }
     // Update is called once per frame
     void Update()
     {
-        if (AIPath.isStopped)
-            StopAtack();
         if (state == State.Patrooling && AIPath.reachedDestination)
             Patroling();
+        if (LogicType == LogicType.distanceMob && (state == State.Atack||state==State.MoveToPoint))
+        {
+            LookAtPlayer();
+        }
+        else AIPath.enableRotation = true;
+        if (LogicType == LogicType.distanceMob && state == State.Atack && (player.transform.position - gameObject.transform.position).sqrMagnitude <= runningAwayDistance* runningAwayDistance)
+        {
+            state = State.MoveToPoint;
+            var point = patroolPoints.Where(x=>(x.transform.position - player.transform.position).sqrMagnitude> runningAwayDistance * runningAwayDistance).OrderBy(x => Math.Abs((x.transform.position - gameObject.transform.position).magnitude)).First();
+            AIDestinationSetter.target = point.transform;
+            AIPath.endReachedDistance = baseDistance;
+        }
+        if (state == State.MoveToPoint && LogicType == LogicType.distanceMob && (player.transform.position - gameObject.transform.position).sqrMagnitude > (runningAwayDistance+ stoprunAwatDelta) * (runningAwayDistance+ stoprunAwatDelta))
+        {
+            AIDestinationSetter.target = player.transform;
+            state = State.Atack;
+            AIPath.endReachedDistance = distanceToPlayer;
+        }
+        if(state==State.Atack&&LogicType==LogicType.flyingMob&& (player.transform.position - gameObject.transform.position).sqrMagnitude <= runningAwayDistance * runningAwayDistance)
+        {
+            if (flyingPoints.Length != 0)
+                AIDestinationSetter.target = flyingPoints[UnityEngine.Random.Range(0, flyingPoints.Length)].transform;
+            state = State.MoveToPoint;
+        }
+        if (state == State.MoveToPoint && LogicType == LogicType.flyingMob && AIPath.reachedEndOfPath)
+        {
+            AIDestinationSetter.target = player.transform;
+            state = State.Atack;
+        }
     }
 }
